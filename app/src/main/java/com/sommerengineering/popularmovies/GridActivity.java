b5a4b4ff79cf3101517c4b5bbbcae524a51ca595
Drew Sommer <andrew.p.sommer@gmail.com>
2018-12-02 14:34:12 -0800

    Check internet connection prior to network call

--- app/src/main/java/com/sommerengineering/popularmovies/GridActivity.java
+++ app/src/main/java/com/sommerengineering/popularmovies/GridActivity.java
@@ -1,11 +1,17 @@
 package com.sommerengineering.popularmovies;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,6 +27,8 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
     private RecyclerView mMovieGrid;
     private ArrayList<MovieObject> mMovies;
     private GridLayoutManager mGridLayoutManager;
+    private ProgressBar mProgressBar;
+    private TextView mErrorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,8 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
         mMovies = new ArrayList<>();
         mAdapter = new MovieAdapter(this, TOTAL_NUMBER_OF_MOVIES, mMovies);
         mGridLayoutManager = new GridLayoutManager(this, 2);
+        mProgressBar = findViewById(R.id.pb_progress);
+        mErrorTextView = findViewById(R.id.tv_error);
 
         // associate the layout manager and adapter to the recycler view
         mMovieGrid.setLayoutManager(mGridLayoutManager);
@@ -43,13 +53,25 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
         // explicitly identifying this to the OS allows for performance optimizations
         mMovieGrid.hasFixedSize();
 
-        // TODO check internet connectivity first here ...
+        // check for internet connectivity
+        if (isConnected()) {
 
-        // initialize a loader manager to handle a background thread
-        LoaderManager loaderManager = getLoaderManager();
+            // initialize a loader manager to handle a background thread
+            LoaderManager loaderManager = getLoaderManager();
 
-        // this initialization causes the OS to call onCreateLoader()
-        loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
+            // this initialization causes the OS to call onCreateLoader()
+            loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
+
+        }
+        else { // no internet connection
+
+            // hide the progress bar
+            mProgressBar.setVisibility(View.GONE);
+
+            // the articles list is empty
+            mErrorTextView.setText(R.string.no_internet_connection);
+
+        }
 
     }
 
@@ -72,10 +94,8 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
         // clear the adapter of any previous query results
         mAdapter.clear();
 
-        // associate the adapter with the recycler grid
-        //mMovieGrid.setAdapter(mAdapter);
-
-        // TODO progress bar visibility
+        // hide the progress bar
+        mProgressBar.setVisibility(View.GONE);
 
         // check the input exists and is not empty
         if (movies != null && !movies.isEmpty()) {
@@ -86,21 +106,22 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
         }
         else {
 
-//            // this conditional handles the rare edge case of (1) successful network call (2) populate ListView
-//            // (3) leave app (4) lose internet connection (5) return to app
-//            if (isConnected()) {
-//
-//                // the articles list is empty because there are no articles matching the search criteria
-//                mEmptyTextView.setText(R.string.no_articles_found);
-//
-//            }
-//
-//            // internet connection was lost after a loader with ARTICLE_LOADER_ID was successfully completed
-//            else {
-//
-//                // the articles list is empty because there is no internet connection
-//                mEmptyTextView.setText(R.string.no_internet_connection);
-//            }
+            // this conditional handles the rare edge case of the following sequence:
+            // (1) successful network call (2) populate recycler grid
+            // (3) leave app (4) lose internet connection (5) return to app
+            if (isConnected()) {
+
+                // the movie list is empty because no match with search criteria
+                mErrorTextView.setText(R.string.no_movies_found);
+
+            }
+
+            // internet connection was lost after a loader successfully completed
+            else {
+
+                // the movie list is empty because there is no internet connection
+                mErrorTextView.setText(R.string.no_internet_connection);
+            }
 
         }
 
@@ -112,6 +133,25 @@ public class GridActivity extends AppCompatActivity implements LoaderManager.Loa
 
         // removing all data from adapter automatically clears the UI listview
         mMovieGrid.setAdapter(null);
+    }
+
+    // check status of internet connectivity
+    public boolean isConnected() {
+
+        // get internet connectivity status as a boolean
+        ConnectivityManager connectivityManager =
+            (ConnectivityManager) getApplicationContext()
+            .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // get network metadata
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+        // boolean representing internet is connected, or in progress connecting
+        boolean isConnected =
+            (activeNetwork != null) && activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
+
     }
 
 }
