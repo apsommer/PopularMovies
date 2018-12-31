c93995485776aecc6e7d298e65b317fb92bbd3d4
Drew Sommer <andrew.p.sommer@gmail.com>
2018-12-30 22:36:16 -0800

    Continue with Room implementation by adding DAO interface

--- app/src/main/java/com/sommerengineering/popularmovies/FavoritesDao.java
+++ app/src/main/java/com/sommerengineering/popularmovies/FavoritesDao.java
@@ -1,4 +1,28 @@
 package com.sommerengineering.popularmovies;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
 public interface FavoritesDao {
+
+    @Query("SELECT * FROM movies ORDER BY mTitle")
+    List<MovieObject> loadAllFavoriteMovies();
+
+    @Insert
+    void insertFavoriteMovie(MovieObject movie);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateFavoriteMovie(MovieObject movie);
+
+    @Delete
+    void deleteFavoriteMovie(MovieObject movie);
+
 }
--- app/src/main/java/com/sommerengineering/popularmovies/FavoritesDatabase.java
+++ app/src/main/java/com/sommerengineering/popularmovies/FavoritesDatabase.java
@@ -43,4 +43,6 @@ public abstract class FavoritesDatabase extends RoomDatabase {
         return sInstance;
     }
 
+    public abstract FavoritesDao favoritesDao();
+
 }
